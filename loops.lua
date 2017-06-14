2a7894a55a7ece4eaa9967e7c633cb4a9c5c4fb7
Sheersho Pramanik <sheersho.pramanik@gmail.com>
2017-06-14 23:46:50 +0530

    used the # operator to get the size of tables

--- loops.lua
+++ loops.lua
@@ -12,3 +12,13 @@ end
 for i=1, 10, 1 do
 print(i)
 end
+
+--[[Simple For loop which adds height of students]]--
+--[[ It also uses # operator for getting the size of the table]]--
+
+student_height = {103, 110, 132, 128, 130}
+sum = 0
+for i=1, #student_height, 1 do
+sum = sum + student_height[i]
+end
+print("The average height between these students is ", sum / #student_height)

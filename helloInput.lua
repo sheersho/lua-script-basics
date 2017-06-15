4401ed8d8ec8171ec3389a876da4b13e7e0b7862
Sheersho Pramanik <sheersho.pramanik@gmail.com>
2017-06-15 23:07:35 +0530

    added a comment

--- helloInput.lua
+++ helloInput.lua
@@ -1,4 +1,4 @@
 text = "\t\t   Hello"
 print("Please enter you name:")
-name = io.read()
+name = io.read() --[[ We Have used the io.read function which basically is calling the read function from the io module]]--
 print(text .. " " .."there "..name.."\n\t\tWelcome to the World of Lua")
